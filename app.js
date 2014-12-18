82eac5e9ec4a5628f53e60f4609c64b69f7e403d
Branko Krstic <saicoder.net@gmail.com>
2014-12-18 11:03:44 +0100

    First version

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require("express")
+
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var request = require('request-json');
+var client = request.newClient('http://gdata.youtube.com/');
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+var queue = [];
+
+
+app.get("/", function(req,res){
+    res.render("index");
+});
+
+
+app.post("/",function(req, res){
+    res.end("fuck " + req.body + req.params.test);
+});
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  
+  socket.on('chat message', function(t){
+      var video_id = t.url;
+      
+      client.get('feeds/api/videos/' + video_id + '?v=2&alt=jsonc', function(err, res, body){
+        if(err) return;   
+        var song = {
+          url: video_id,
+          title: body.data.title,
+          image: body.data.thumbnail.sqDefault,
+          duration: body.data.duration
+        }
+        queue.push(song);
+        io.emit("queue.changed", queue);
+      });
+      
+      
+  }); 
+    
+  io.emit("queue.changed", queue);
+});
+
+
+
+http.listen(3000, '0.0.0.0', function(){
+  console.log('listening on *:3000');
+});
