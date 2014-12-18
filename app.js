b303a73df2883ebb6367cec4d920069865a8f4b0
Branko Krstic <saicoder.net@gmail.com>
2014-12-18 11:22:15 +0100

    proc file fixes

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ io.on('connection', function(socket){
 
 
 
-http.listen(process.env.PORT || 3000, '0.0.0.0', function(){
+http.listen(80, '0.0.0.0', function(){
   console.log('listening on *:3000');
 });
